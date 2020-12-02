37d6215605b83ef715ecc14ff1a353aa2b2b11e7
Bruno Rafael <brunorvl@gmail.com>
2020-12-02 09:40:54 -0300

    Ajuste no bot para rolagem de dados com o calculo de bonus ou dificuldade

--- src/commands/teste.js
+++ src/commands/teste.js
@@ -8,9 +8,13 @@ module.exports = {
     execute(message, args) {
         var jogada = args[0].toLowerCase().trim();
         if (jogada.indexOf('d') > 0) {
-            if (jogada.indexOf('+') > 0 || jogada.indexOf('-') > 0) {
+            console.log('mais:');
+            console.log(jogada.indexOf('+'));
+            console.log('menos:');
+            console.log(jogada.indexOf('-'));
+            if (jogada.indexOf('+') >= 0) {
                 var jogadatexto = jogada; 
-                if (jogada.indexOf('+') > 0) { jogada = jogada.split('+'); } else { jogada = jogada.split('-'); }                
+                jogada = jogada.split('+');                
                 var dado = jogada[0];
                 var soma = jogada[1];
                 dado = dado.split('d');
@@ -20,7 +24,7 @@ module.exports = {
                 var resultado = 0;
                 var cont = 1;
                                 
-                if (parseInt(dado[0]) > 0) { cont = parseInt(dado[0]); }
+                if (parseInt(dado[0]) >= 0) { cont = parseInt(dado[0]); }
                 
                 for(var i=0;i< cont;i++){
                     ram = Math.floor(Math.random() * parseInt(dado[1])) + 1;
@@ -31,13 +35,46 @@ module.exports = {
                         calculo = calculo +" + "+ram.toString()+" ";
                     }
                 }
-                if (jogada.indexOf('+') > 0) {
-                    calculo = calculo +" + ("+soma.toString()+") ";
-                    resultado += parseInt(soma); 
-                } else { 
-                    calculo = calculo +" - ("+soma.toString()+") ";
-                    resultado -= parseInt(soma); 
+                
+                calculo = calculo +" + ("+soma.toString()+") ";
+                resultado += parseInt(soma); 
+                
+
+                let jogadadedados = new MessageEmbed()
+                    .setTitle(`O Oráculo pressente que...`)
+                    .setDescription(`🎲  ${jogadatexto}:: ${calculo} = ${resultado}`)
+                    .setColor("GREEN")
+                
+                return message.channel.send(jogadadedados);
+
+
+            } else if (jogada.indexOf('-') >= 0) {
+                var jogadatexto = jogada; 
+                jogada = jogada.split('-');                
+                var dado = jogada[0];
+                var soma = jogada[1];
+                dado = dado.split('d');
+                
+                var ram = 0;
+                var calculo = " ";
+                var resultado = 0;
+                var cont = 1;
+                                
+                if (parseInt(dado[0]) >= 0) { cont = parseInt(dado[0]); }
+                
+                for(var i=0;i< cont;i++){
+                    ram = Math.floor(Math.random() * parseInt(dado[1])) + 1;
+                    resultado += ram;
+                    if (i == 0) {
+                        calculo = calculo+""+ram.toString();
+                    } else {
+                        calculo = calculo +" + "+ram.toString()+" ";
+                    }
                 }
+                
+                calculo = calculo +" - ("+soma.toString()+") ";
+                resultado -= parseInt(soma); 
+                
 
                 let jogadadedados = new MessageEmbed()
                     .setTitle(`O Oráculo pressente que...`)
